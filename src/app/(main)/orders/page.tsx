b03d22af04c208b5d0694c148b85bb8aec350b8a
Ph2006 <117409323+Ph2006@users.noreply.github.com>
2025-09-16 15:16:45 -0300

    Update page.tsx

--- src/app/(main)/orders/page.tsx
+++ src/app/(main)/orders/page.tsx
@@ -4706,7 +4706,7 @@ export default function OrdersPage() {
 
 
     return (
-    <div className="w-full">
+        <div className="w-full">
             <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
                 <div className="flex items-center justify-between space-y-2">
                     <h1 className="text-3xl font-bold tracking-tight font-headline">Pedidos de Produção</h1>
