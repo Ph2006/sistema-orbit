151095d9f1b47f1f58df6b5bfe30d9f9b459495e
Ph2006 <117409323+Ph2006@users.noreply.github.com>
2025-09-16 15:02:14 -0300

    Update page.tsx

--- src/app/(main)/orders/page.tsx
+++ src/app/(main)/orders/page.tsx
@@ -4705,7 +4705,7 @@ export default function OrdersPage() {
 
 
 
-return (
+    return (
     <div className="w-full">
             <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
                 <div className="flex items-center justify-between space-y-2">
