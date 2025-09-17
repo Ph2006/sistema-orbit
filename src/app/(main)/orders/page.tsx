c0cb07e797ac5c958fa63df82e36420eaa10b9fd
Ph2006 <117409323+Ph2006@users.noreply.github.com>
2025-09-17 14:51:02 -0300

    Update page.tsx

--- src/app/(main)/orders/page.tsx
+++ src/app/(main)/orders/page.tsx
@@ -4205,8 +4205,9 @@ export default function OrdersPage() {
 
 
 
-return (
-    <div className="w-full">
+    return (
+        <>
+            <div className="w-full">
             <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
                 <div className="flex items-center justify-between space-y-2">
                     <h1 className="text-3xl font-bold tracking-tight font-headline">Pedidos de Produção</h1>
@@ -5824,6 +5825,7 @@ return (
                 </AlertDialogFooter>
               </AlertDialogContent>
             </AlertDialog>
-        </div>
+            </div>
+        </>
     );
 }
