f782dea8035c4a7ff8cb905ff6b664fc3c21b366
Ph2006 <117409323+Ph2006@users.noreply.github.com>
2026-08-21 16:21:55 -0300

    Delete src/app/cronograma/[[...params]] directory

--- src/app/cronograma/[[...params]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import CronogramaPublicoClient from "./client";
-
-export function generateStaticParams() {
-  return [];
-}
-
-export default function CronogramaPage() {
-  return <CronogramaPublicoClient />;
-}
